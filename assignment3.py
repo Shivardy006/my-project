a81578b348d5222098850e3d015b992ec2faad51
Shiva Reddy <shivareddyb006@gmail.com>
2026-05-04 23:18:31 +0530

    Assignment-4

---
+++ assignment3.py
@@ -0,0 +1,211 @@
+# Assignment 3:
+
+# 1.Write a program to demonstrate all arithmetic operators using two user-defined numbers.
+
+# Program:
+a = float(input("Enter a value: "))
+b = float(input("Enter b value: "))
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Exponent:", round(a ** b, 5))
+if b==0:
+    print ("Divisor should not be zero")
+    print("Try another number to perform modulus, Division and Floor Division")
+else:
+    print("Modulus:", a % b)
+    print("Division:", a / b)
+    print("Floor Division:", a // b)
+
+'''Output:
+Enter first number: 5.5
+Enter second number: 2.5
+Addition: 8.0
+Subtraction: 3.0
+Multiplication: 13.75
+Exponent: 70.94254
+Modulus: 0.5
+Division: 2.2
+Floor Division: 2.0'''
+
+
+# 2.Write a program to compare two numbers using relational operators and display the result.
+
+# Program:
+a = int(input("Enter a value : "))
+b = int(input("Enter b value: "))
+print("a == b:", a == b)
+print("a != b:", a != b)
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a >= b:", a >= b)
+print("a <= b:", a <= b)
+
+'''Output:
+Enter a value : 4
+Enter b value: 4
+a == b: True
+a != b: False
+a > b: False
+a < b: False
+a >= b: True
+a <= b: True'''
+
+
+# 3.Explain logical operators (and, or, not) with a truth table and a sample program.
+'''
+Truth Table:
+____________________________________________
+| A     | B     | A and B | A or B | not A |
+| ----- | ----- | ------- | ------ | ----- |
+| True  | True  | True    | True   | False |
+| True  | False | False   | True   | False |
+| False | True  | False   | True   | True  |
+| False | False | False   | False  | True  |
+
+Program:'''
+a = True
+b = False
+print("a and b:", a and b)
+print("a or b:", a or b)
+print("not a:", not a)
+
+'''Output:
+a and b: False
+a or b: True
+not a: False'''
+
+
+# 4.Write a program to check whether a number lies between 10 and 50 using logical operators.
+
+# Program:
+num = int(input("Enter a number: "))
+if num > 10 and num < 50:
+    print("Number is between 10 and 50")
+else:
+    print("Number is NOT between 10 and 50")
+
+'''Output:
+Enter a number: 25
+Number is between 10 and 50'''
+
+
+# 5.Demonstrate the use of assignment operators (=, +=, -=, *=, /=) with examples.
+
+# Program:
+x = 10
+print("Initial value:", x)
+x += 5
+print("After += :", x)
+x -= 3
+print("After -= :", x)
+x *= 2
+print("After *= :", x)
+x /= 4
+print("After /= :", x)
+
+'''Output:
+Initial value: 10
+After += : 15
+After -= : 12
+After *= : 24
+After /= : 6.0
+'''
+
+# 6.Write a program to check whether a given value exists in a list using membership operators.
+
+# Program:
+lst = [10, 20, 30, 40, 50]
+value = int(input("Enter value to search: "))
+if value in lst:
+    print("Value exists in list")
+else:
+    print("Value does not exist in list")
+
+'''Output:
+Enter value to search: 30
+Value exists in list'''
+
+
+''' 7.What is the difference between == and is operators? Write a program to prove it.
+
+== -> checks value equality (are the contents the same)
+is -> checks identity, compares memory locations (are they the same object in memory)
+
+Program:'''
+# Same values, different objects
+a = [1, 2, 3]
+b = [1, 2, 3]
+print("a == b:", a == b)  # True (values are equal)
+print("a is b:", a is b)  # False (different memory locations)
+
+# Same object
+c = a
+print("a == c:", a == c)  # True
+print("a is c:", a is c)  # True (same object)
+
+# Small integers (special case: storing Cache )
+x = 10
+y = 10
+print("x == y:", x == y)  # True
+print("x is y:", x is y)  # True (Python reuses memory)
+
+'''Output:
+a == b: True
+a is b: False
+a == c: True
+a is c: True
+x == y: True
+x is y: True
+'''
+
+# 8.Write a program to check whether a number is even or odd using operators.
+
+# Program:
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+'''Output:
+Enter a number: 6
+Even number
+'''
+
+# 9.Write a program to calculate the area of a rectangle using arithmetic operators and user input.
+
+# Program:
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+area = length * width
+print("Area of rectangle:", area)
+
+'''Output:
+Enter length: 6
+Enter width: 4
+Area of rectangle: 24.0'''
+
+
+'''10.Explain operator precedence in Python and write a program showing how precedence affects the result.
+
+Operator precedence determines which operation is performed first.
+
+Order is high to low:
+1. () (parentheses)
+2. ** (exponent)
+3. *, /, //, % (These are evaluated from left to right)
+4. +, -
+5. Comparison operators (==, !=, etc.)
+6. Logical operators (and, or, not)
+
+Program:'''
+res = 10 + 5 * 2
+result = (10 + 5) * 2
+print("Without parentheses:", res)
+print("With parentheses:", result)
+
+'''Output:
+Without parentheses: 20
+With parentheses: 30
+'''
---
+++ assignment4.py
@@ -0,0 +1,181 @@
+# Assignment 4:
+'''
+Conditional statements:
+
+1.Write a program to check whether a given number is positive, negative, or zero.
+
+Program:'''
+num = float(input("Enter a number: "))
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+'''Output:
+Enter a number: 4
+The number is positive.
+'''
+
+# 2.Write a program to check whether a given number is even or odd using an if-else statement.
+
+# Program:
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+'''Output:
+Enter a number: 8
+The number is even.
+'''
+
+# 3.Write a program to find the largest of two numbers entered by the user.
+
+# Program:
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+if num1 > num2:
+    print("The largest number is", num1)
+else:
+    print("The largest number is", num2)
+
+'''Output:
+Enter first number: 6
+Enter second number: 4
+The largest number is 6.0'''
+
+
+# 4.Write a program to find the largest of three numbers using if-elif-else.
+
+# Program:
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+if num1 >= num2 and num1 >= num3:
+    print("The largest number is", num1)
+elif num2 >= num1 and num2 >= num3:
+    print("The largest number is", num2)
+else:
+    print("The largest number is", num3)
+
+'''Output:
+Enter first number: 8
+Enter second number: 5
+Enter third number: 9
+The largest number is 9.0'''
+
+
+# 5.Write a program to check whether a given year is a leap year or not.
+
+# Program:
+year = int(input("Enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
+
+'''Output:
+Enter a year: 2024
+2024 is a leap year
+'''
+
+# 6.Write a program to check whether a person is eligible to vote based on age input.
+
+# Program:
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+'''Output:
+Enter your age: 16
+You are not eligible to vote.
+'''
+
+''' 7.Write a program to display student grade based on the following conditions:
+
+    Marks ≥ 90 → Grade A
+
+    Marks ≥ 75 → Grade B
+
+    Marks ≥ 60 → Grade C
+
+    Marks < 60 → Grade D
+
+Program:'''
+marks = float(input("Enter your marks: "))
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+else:
+    grade = "D"
+print("Your grade is", grade)
+
+'''Output:
+Enter your marks: 96
+Your grade is A
+'''
+
+# 8.Write a program to check whether a given character is: a vowel or a consonant
+
+# Program:
+char = input("Enter a single character: ").lower()
+if char in 'aeiou':
+    print("The character is a vowel")
+else:
+    print("The character is a consonant")
+
+'''Output:
+Enter a single character: i
+The character is a vowel
+'''
+
+'''9.Write a program to create a simple calculator using if- elif -else
+(addition, subtraction, multiplication, division).
+
+Program:'''
+a = float(input("Enter a value: "))
+b = float(input("Enter b value: "))
+operation = input("Enter operation (+, -, *, /): ")
+if operation == '+':
+    print("Result:", a + b)
+elif operation == '-':
+    print("Result:", a - b)
+elif operation == '*':
+    print("Result:", a * b)
+elif operation == '/':
+    if b != 0:
+        print("Result:", a / b)
+    else:
+        print("Error: Division by zero!")
+else:
+    print("Invalid operation.")
+
+'''Output:
+Enter first number: 4
+Enter second number: 0
+Enter operation (+, -, *, /): /
+ERROR!
+Error: Division by zero!
+'''
+
+# 10.Write a program to check whether a given number is a three-digit number or not using conditional statements.
+ 
+# Program:
+num = int(input("Enter an integer value: "))
+if 100 <= abs(num) <= 999:     # negative numbers are handled correctly
+    print(num,"is a three-digit number")
+else:
+    print(num,"is not a three-digit number")
+
+'''Output:
+Enter an integer value: -565
+-565 is a three-digit number
+'''
